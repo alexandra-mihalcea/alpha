ed00f8201ca0f16a8f9f74d21473e9cfc0f9ebf0
alexandra-mihalcea <alexandramihalcea15@gmail.com>
2016-08-02 17:46:19 +0100

    added manifest file

--- js/custom.js
+++ js/custom.js
@@ -388,3 +388,21 @@ function checkAnswer(button_id,x){
     }
     return(correct);
 }
+
+
+//update app
+function checkForUpdate()
+{
+    if (window.applicationCache != undefined && window.applicationCache != null)
+    {
+        window.applicationCache.addEventListener('updateready', updateApplication);
+    }
+}
+
+function updateApplication(event)
+{
+    if (window.applicationCache.status != 4) return;
+    window.applicationCache.removeEventListener('updateready', updateApplication);
+    window.applicationCache.swapCache();
+    window.location.reload();
+}
